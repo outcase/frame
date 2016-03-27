af841f0a867485411193561a031c6569fad07c6f
outcase <anatoly@molchanov.su>
2016-03-27 03:50:11 +0300

    test7

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -20,7 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-	this.ref();
+//	this.ref();
     },
     // Bind Event Listeners
     //
@@ -38,12 +38,232 @@ var app = {
         app.receivedEvent('deviceready');
     },
 
-ref: function(){
-window.open('http://apache.org', '_blank', 'location=no');
-addEventListener('loadstart', function() { alert(event.url); });
-alert(this);
-this.show();
-return this;},
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
+//ref: function(){
+//window.open('http://apache.org', '_blank', 'location=no');
+//addEventListener('loadstart', function() { alert(event.url); });
+//alert(this);
+//this.show();
+//return this;},
+
 
 
     // Update DOM on a Received Event
